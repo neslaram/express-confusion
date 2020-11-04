89061a5d97b63cb636b46db2ceb27db24162b6f7
Néstor Lara <nesla.92@hotmail.com>
2020-11-04 01:45:36 -0600

    Express Session Part 1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
