9e0d70d3fc89bc17622d966b27f8aa71bc6c587a
Néstor Lara <nesla.92@hotmail.com>
2020-11-08 13:42:54 -0600

    Passport

--- app.js
+++ app.js
@@ -59,15 +59,3 @@ app.use(function (err, req, res) {
 });
 
 module.exports = app;
-
-function auth (req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-    const err = new Error('You are not authenticated!');
-    err.status = 403;
-    next(err);
-  } else {
-    next();
-  }
-}
